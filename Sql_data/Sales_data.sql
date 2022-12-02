3a12dfe16dac7dff500c518105ac4d1005c20ffc
murillowilmar1 <murillowilmar1@homtail.com>
2022-12-02 11:30:50 -0500

    sql

---
+++ Sql_data/Sales_data.sql
@@ -0,0 +1,9 @@
+SELECT "Area Code","State",  "Market", "Market Size", "Profit", "Margin", "Sales", "COGS", "Total Expenses",
+"Marketing", "Inventory", "Budget Profit", "Budget COGS", "Budget Margin", "Budget Sales", "ProductId",
+"Date", "Product Type", "Product", "Type"
+
+
+
+FROM sales_data 
+
+
