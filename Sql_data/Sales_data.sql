954946ed9121c840a91da5af92b001f8c6f62f90
murillowilmar1 <murillowilmar1@homtail.com>
2022-12-02 11:34:42 -0500

    sql1

--- Sql_data/Sales_data.sql
+++ Sql_data/Sales_data.sql
@@ -1,4 +1,5 @@
-SELECT "Area Code","State",  "Market", "Market Size", "Profit", "Margin", "Sales", "COGS", "Total Expenses",
+SELECT 
+"Area Code","State",  "Market", "Market Size", "Profit", "Margin", "Sales", "COGS", "Total Expenses",
 "Marketing", "Inventory", "Budget Profit", "Budget COGS", "Budget Margin", "Budget Sales", "ProductId",
 "Date", "Product Type", "Product", "Type"
 
